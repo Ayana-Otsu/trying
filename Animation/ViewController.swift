fec0b3bdc81d991be5a6c0a209345e5d97bca671
ayana_otsu <ayana.otsu5@gmail.com>
2022-05-06 22:17:31 +0900

    Initial Commit

---
+++ Animation/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  Animation
+//
+//  Created by Ayana Otsu on 2022/05/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func buttonTapped(sender: UIButton) {
+        
+//        UIView.animate(withDuration: 1.0) {
+            //1.0秒かけるアニメーションの終了後の記述。
+//            self.imageView.transform = CGAffineTransform(translationX: 0, y: -100)
+//       }
+        
+        UIView.animate(withDuration: 1.0) {
+            self.imageView.transform = CGAffineTransform(translationX: 0, y: -100)
+        } completion: { _ in
+            
+        }
+
+        }
+    }
+
+
+}
+
