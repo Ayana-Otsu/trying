ff5dc0b92deb8354f0661e91cccc5fd8f579e1b8
ayana_otsu <ayana.otsu5@gmail.com>
2022-05-06 22:31:21 +0900

    I changed translationX: 20 to translationX: 30

--- Animation/ViewController.swift
+++ Animation/ViewController.swift
@@ -23,7 +23,7 @@ class ViewController: UIViewController {
 //       }
         
         UIView.animate(withDuration: 1.0) {
-            self.imageView.transform = CGAffineTransform(translationX: 20, y: -100)
+            self.imageView.transform = CGAffineTransform(translationX: 30, y: -100)
         } completion: { _ in
             
         }
@@ -32,5 +32,5 @@ class ViewController: UIViewController {
     }
 
 
-}
+
 
